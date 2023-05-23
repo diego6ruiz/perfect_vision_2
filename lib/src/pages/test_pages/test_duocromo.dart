09b8ff948fb50a9c5cc4b2246bbc16a429444509
diego6ruiz <diego6ruiz@gmail.com>
2023-05-22 21:44:15 -0600

    Padding tests

--- lib/src/pages/test_pages/test_duocromo.dart
+++ lib/src/pages/test_pages/test_duocromo.dart
@@ -21,48 +21,53 @@ class DuocromoPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container( 
+      body: Container(
         color: Colors.red,
-        child: Column(
-          children: [
-            SizedBox(height: MediaQuery.of(context).padding.top),
-            Expanded(
-              child: SingleChildScrollView(
-                physics: NeverScrollableScrollPhysics(),
-                child: Column(
-                  children: [
-                    for (var i = 1; i <= 9; i++)
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: List.generate(
-                          i+2,
-                          (index) {
-                            final fontSize = screenWidth / (i * 2) - (i == 8 ? 8 : 0);
-                            return Expanded(
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: FittedBox(
-                                  fit: BoxFit.scaleDown,
-                                  child: Text(
-                                    textAlign: TextAlign.center,
-                                    String.fromCharCode(Random().nextInt(26) + 65),
-                                    style: TextStyle(
-                                      fontFamily: 'OpticianSans',
-                                      fontSize: fontSize,
-                                      fontWeight: FontWeight.bold,
-                                    ), 
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
+          child: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).padding.top),
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: NeverScrollableScrollPhysics(),
+                  child: Column(
+                    children: [
+                      for (var i = 1; i <= 9; i++)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: List.generate(
+                            i + 2,
+                            (index) {
+                              final fontSize =
+                                  screenWidth / (i * 2.4) - (i >= 8 ? 8 : 0);
+                              return Expanded(
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: FittedBox(
+                                    fit: BoxFit.scaleDown,
+                                    child: Text(
+                                      textAlign: TextAlign.center,
+                                      String.fromCharCode(
+                                          Random().nextInt(26) + 65),
+                                      style: TextStyle(
+                                        fontFamily: 'OpticianSans',
+                                        fontSize: fontSize,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
